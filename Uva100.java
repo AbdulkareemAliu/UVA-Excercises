7951d11006a8e3d9d5ccf8c444255dc160b4670a
Abdulkareem Aliu <abdulkareemaliu@gmail.com>
2019-04-07 17:12:07 -0400

    Moved the code from the main method to a seperate one.

--- Uva100.java
+++ Uva100.java
@@ -1,17 +1,21 @@
 public class Uva100{
-	public static void main(String[] args) {
-		int n = Integer.parseInt(args[0]);
-		System.out.print(n + " ");
-		 while (n != 1){
-			if (n%2 == 0){
-				n = n/2;
-			} else {
-				n = (3*n)+1;
-			}
+	
+	public static void gen3nPlus1(int n){
+			System.out.print(n + " ");
+		 	while (n != 1){
+				if (n%2 == 0){
+					n = n/2;
+				} else {
+					n = (3*n)+1;
+				}
 			System.out.print(n + " ");
 
-		}
-		System.out.println();
-
+			}
+			System.out.println();
+	
+	}
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		gen3nPlus1(n);
 	}
 }
